d0dd561fa96298eb5107befc89629ecd65e8cc84
BAGUS0630 <mbagus0111@gmail.com>
2024-11-13 19:54:56 +0700

    ad

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const modal = document.getElementById("qrismodal");
     const btn = document.getElementById("paymentButton");
     const span = document.getElementsByClassName("close")[0];
-}
+
+    btn.onclick = function(){
+        modal.style.display = "block";
+    };

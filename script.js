53f3c8c44c482525ff0f2bc9859100e79edab5fa
BAGUS0630 <mbagus0111@gmail.com>
2024-11-14 21:49:09 +0700

    add

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal= document.getElementById("qrismodal");
-    var btn = document.getElementById("paymentButton");
-    var span = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function(){
-        modal.style.display = "flex";
-    };
+var modal = document.getElementById("qrisModal");
+var btn = document.getElementById("paymentButton");
+var span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
 
-    window.onclick = function(event) {
-        if (event.target == modal){
-            modal.style.display ="none";
-        }
-    };
-});    
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
